ca2980035d9911da43eebb037ee3100eac43be9b
raghul <raghulsrinivas20201@gmail.com>
2021-07-03 11:46:50 +0530

    second comit

--- default argument.py
+++ default argument.py
@@ -1,5 +1,5 @@
 #default argument takes the default value defined in the function defenition if there is no arguments passed in the funcyion call
-def my_fun(name="raghul"):
+def my_fun(name="ragh"):
      print(f"{name} has got the first marks in the class")
 #If i have not pass a parameter in function call it will print the default value in the function defenition     
 my_fun("srinivs")
